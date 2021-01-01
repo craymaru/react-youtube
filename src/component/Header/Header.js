5cb590b2085ddee0490aa0f782920f602af6ba86
Cray <craymaru@gmail.com>
2021-01-01 20:39:37 +0900

    added header title and form with css

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+import Style from './Header.module.scss'
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        Header
-      </header>
-    </div>
+    <header className={Style.header}>
+      <div className={Style.item}>
+        <Link to="/">MeowTube</Link>
+      </div>
+      <div className={Style.item}>
+        <form action="">
+          <input type="text" placeholder="Search"/>
+          <button type="submit">
+            <FontAwesomeIcon icon={faSearch}/>
+          </button>
+        </form>
+      </div>
+    </header>
   )
 }
 
