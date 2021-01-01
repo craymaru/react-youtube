b8a5626f621ef820838cb26b93ff2e96df2e5aaf
Cray <craymaru@gmail.com>
2021-01-01 18:28:37 +0900

    set prettier and adapt it

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import Top from './pages/Top'
 import Search from './pages/Search'
 import Watch from './pages/Watch'
 
-
 function App() {
   return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Top}/>
-          <Route exact path="/search"  component={Search}/>
-          <Route exact path="/watch" component={Watch}/>
-        </Switch>
-      </Router>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Top} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/watch" component={Watch} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
