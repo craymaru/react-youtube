1e5d07539c6a292a22517d7d6bec071c03701f8d
Cray <craymaru@gmail.com>
2021-01-02 18:23:44 +0900

    remove unnecessary react import

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from "../Header/Header";
 import Style from './Layout.module.scss'
 
--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import { useEffect, useContext } from 'react'
 
 import Layout from '../component/Layout/Layout'
 import { fetchPopularData } from '../apis/index'
